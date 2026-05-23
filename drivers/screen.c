71aa94ee25d4eea81251dfe3d4a590d0513ab4e2
Akarwash <akarsh.jain.8ra@gmail.com>
2026-05-22 22:50:42 -0700

    started working on the screen stuff with claude but realized theres a lot more i need to learn before i start getting into it

---
+++ drivers/screen.c
@@ -0,0 +1,11 @@
+#include "screen.h"
+#include "ports.h"
+#include "../libc/mem.h"
+
+static int get_cursor(void) {
+    port_byte_out(0x3D4, 14);
+    int offset = port_byte_in(0x3D5) << 8;
+    port_byte_out(0x3D4, 15);
+    offset += port_byte_in(0x3D5);
+    return offset;
+}
---
+++ drivers/screen.h
@@ -0,0 +1,16 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "../include/types.h"
+
+#define VIDEO_ADDRESS   0xB8000
+#define MAX_ROWS        25
+#define MAX_COLS        80
+#define WHITE_ON_BLACK  0x0F
+
+// public functions
+void screen_clear(void);
+void print_char(char c);
+void print_string(char *str);
+
+#endif
