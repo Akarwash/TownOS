fa39095b9592b0e8c80de88d66e80595f8d1ee9b
Akarwash <akarsh.jain.8ra@gmail.com>
2026-05-25 10:30:22 -0700

    string files started to get to the shell stuff

---
+++ libc/string.h
@@ -0,0 +1,10 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include "../include/types.h"
+
+size_t strlen(char *str);
+int strcmp(char *s1, char *s2);
+char *strcpy(char *dest, const char *src);
+
+#endif
